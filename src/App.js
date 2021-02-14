af1da72e430bacecfe56126a0bf97599fb972b5c
Marius-De-Allie <spectre4300s@yahoo.com>
2021-02-13 23:08:53 -0500

    Update HomePage component content and styling

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
      <HomePage />
-     <ServicesPage />
+     {false && <ServicesPage />}
     </div>
   );
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 const HomePage = props => {
   return (
-    <div>
-      HOMEPAGE
+    <div className="main-container">
+      <p className="landing-txt">Island Style Landscaping, make your dreams a reality</p>
+      <div className="btn-container">
+        <div className="btn">gallery</div>
+        <div className="btn">services</div>
+      </div>
     </div>
   );
 };
