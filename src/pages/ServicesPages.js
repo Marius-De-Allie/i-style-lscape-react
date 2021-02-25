265842842844f18560d870aef5103a83ed906d40
Marius-De-Allie <spectre4300s@yahoo.com>
2021-02-24 22:27:16 -0500

    Iterate over services array returning a p element for each array element

--- src/pages/ServicesPages.js
+++ src/pages/ServicesPages.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { GiSpade, GiHighGrass, GiBrickWall } from 'react-icons/gi';
 import { MdDriveEta } from 'react-icons/md';
+import services from '../utils/data';
 
 const ServicesPage = props => {
 
@@ -9,13 +10,9 @@ const ServicesPage = props => {
       <div className="serv-container">
         <h3 className="serv-header">We specialize in</h3>
         <div className="servlist-container">
-          <p>Demolition</p>
-          <p>Excavation <GiSpade /></p>
-          <p>Landscaping <GiHighGrass /></p>
-          <p>Driveways <MdDriveEta /></p>
-          <p>Foundations <GiBrickWall /></p>
-          <p>Back Filling</p>
-          <p>Grading & Leveling</p>
+          {services.map(service => (
+            <p key={service}>{service}</p>
+          ))}
         </div>
       </div>
     </div>
