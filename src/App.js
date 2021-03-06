1c03b3c15abc0805a4ab06a0c860c7adebbbc0c8
Marius-De-Allie <spectre4300s@yahoo.com>
2021-03-05 19:21:38 -0500

    Create new GalleryPage component and create a Route for GalleryPage in app.js file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Switch, Route} from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ServicesPage from './pages/ServicesPages';
+import GalleryPage from './pages/GalleryPage';
 import Nav from './components/Nav';
 import './App.css';
 
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route path="/" exact component={HomePage} />
         <Route path="/services" component={ServicesPage} />
+        <Route path="/gallery" component={GalleryPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/GalleryPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const GalleryPage = props => {
+
+  return (
+    <div>
+      Gallery
+    </div>
+  );
+
+};
+
+export default GalleryPage;
