938af8176122e36fce7dfb98f42b85f64f445381
Marius-De-Allie <spectre4300s@yahoo.com>
2021-03-08 06:26:46 -0500

    Create ContactPage component

---
+++ src/pages/ContactPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ContactPage = () => (
+  <div className="main-contact-container">
+    <p className="contact-header">Get in touch with us</p>
+    <div className="contact-form">
+    
+    </div>
+    <div className="s-media-container">
+    
+    </div>
+  </div>
+);
+
+export default ContactPage;
