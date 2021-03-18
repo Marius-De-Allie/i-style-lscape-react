2e9b2083cdec7d6625dbcf6a537837368d7573cb
Marius-De-Allie <spectre4300s@yahoo.com>
2021-03-18 10:14:41 -0400

    create handleLogoClick function and assign function to onClick prop of logo in Nav component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, withRouter } from 'react-router-dom';
 
-const Nav = props => {
+const Nav = ({ location, history }) => {
+
+  // Logo click handler.
+  const handleLogoClick = () => {
+    // check if not already on the homepage.
+    if(location.pathname !== '/') {
+      // redirect to homepage.
+      history.push('/');
+    }
+  };
 
   return (
     <div className="nav-con">
-      <div className="logo">LOGO</div>
+      <div 
+        className="logo"
+        onClick={handleLogoClick}
+      >
+        LOGO
+      </div>
       <nav>
         <NavLink to="/" activeClassName="selected" exact>Home</NavLink>
         <NavLink to="/services" activeClassName="selected">Services</NavLink>
@@ -16,4 +30,4 @@ const Nav = props => {
   );
 };
 
-export default Nav;
+export default withRouter(Nav);
